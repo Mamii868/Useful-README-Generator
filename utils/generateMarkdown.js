6b42b957ea875516dca7da7670b31389552cbe24
Marc Moten <marcmotenii@gmail.com>
2021-10-01 16:14:26 -0500

    compressed license information

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,37 +1,25 @@
 const init = require('../index')
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
+function renderLicense(license) {
   if (answers.licenses == "MIT") {
     answers.license = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+    return answers.license
   } else if (answers.license == "GPLv3") {
     answers.license = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+    return answers.license
   } else if (answers.license == "Apache 2.0") {
     answers.license = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+    return answers.license
   } else if (answers.license == "BSD 3") {
     answers.license = "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
+    return answers.license
   } else {
     answers.license = "none"
+    return ''
   }
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) { 
-  if(license) {
-    return license
-  }
-  return ''
-}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) { 
-  return `${renderLicenseBadge} 
-  ${renderLicenseLink}`
-}
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(answers) {
  return `${answers.title}
   
@@ -57,7 +45,7 @@ function generateMarkdown(answers) {
   
   ## License
 
-  ${renderLicenseSection}
+  ${renderLicense}
  
   ## How to Contribute
 
