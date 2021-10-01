c4cf9e5346d07edd77d79de9a7d7625948512642
Marc Moten <marcmotenii@gmail.com>
2021-10-01 15:35:14 -0500

    moved the previous prompts into questions variable and creates a createFile function

--- index.js
+++ index.js
@@ -6,81 +6,75 @@ const renderLicenseSection = require("./utils/generateMarkdown");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 const questions = [
-    "what is the title?",
-    "Give a description of the project",
-    "enter installation instructions, if any",
-    "enter how to use the application",
-    "if available, give instructions on how to contribute",
-    "enter test instructions"
-];
+    {
+        type: "input",
+        message: "what is the title?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Give a description of the project",
+        name: "decription"
+    },
+    {
+        type: "input",
+        message: "enter installation instructions, if any",
+        name: "instructions"
+    },
+    {
+        type: "input",
+        message: "enter how to use the application",
+        name: "howToUse"
+    },
+    {
+        type: "input",
+        message: "if available, give instructions on how to contribute",
+        name: "contributions"
+    },
+    {
+        type: "input",
+        message: "enter test instructions",
+        name: "test"
+    },
+    {
+        type: 'checkbox',
+        message: "what licenses are needed",
+        name: "license",
+        choices: ["MIT", "GPLv3", "Apache 2.0", "BSD 3", "None"]
+    },
+    {
+        type: "input",
+        message: "enter your Github username",
+        name: "user"
+    },
+    {
+        type: "input",
+        message: "enter your email",
+        name: "email"
+    },
+]
 
 function init() {
-    inquirer.prompt(
-    [
-        {
-            type: "input",
-            message: "what is the title?",
-            name: "title"
-        },
-        {
-            type: "input",
-            message: "Give a description of the project",
-            name: "decription"
-        },
-        {
-            type: "input",
-            message: "enter installation instructions, if any",
-            name: "instructions"
-        },
-        {
-            type: "input",
-            message: "enter how to use the application",
-            name: "howToUse"
-        },
-        {
-            type: "input",
-            message: "if available, give instructions on how to contribute",
-            name: "contributions"
-        },
-        {
-            type: "input",
-            message: "enter test instructions",
-            name: "test"
-        },
-        {
-            type: 'checkbox',
-            message: "what licenses are needed",
-            name: "license",
-            choices: ["MIT", "GPLv3", "Apache 2.0", "BSD 3", "None"]
-        },
-        {
-            type: "input",
-            message: "enter your Github username",
-            name: "user"
-        },
-        {
-            type: "input",
-            message: "enter your email",
-            name: "email"
-        }
-
-
-    ]
-)
+    inquirer.prompt(questions)
     .then((answers) => {
-       fs.writeFileSync('./README1.md', 
-       `${generateMarkdown(answers)}`, err => {
-           if(err) {
-               console.log(err)
-               return
-           }
-       })
-    })
+      console.log(answers) })
+    
 .catch((err) => {
     console.log(err)
 })
 }
  
+function createFile(info) {
+    fs.writeFileSync('./README1.md', 
+    info, err => {
+        if(err) {
+            console.log(err)
+            return
+        }else{
+            console.log("created File")
+            return
+        }
+})};
 
 
 // Function call to initialize app
